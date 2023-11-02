88a2cc6f13184a93eb0d502add2fb73d6fbfdad1
LolloVagnoloo <57802876+LolloVagnoloo@users.noreply.github.com>
2023-11-02 14:41:19 +0100

    COMMIT FINALE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,30 +83,7 @@
         @endforeach
     </div>
     <!-- SCRIPT JS PER FAR SCORRERE LE FOTO -->
-    <script>
-        let photoArray = [
-    'http://127.0.0.1:8000/build/assets/fano.jpg',
-    'http://127.0.0.1:8000/build/assets/fano1.jpg',
-    'http://127.0.0.1:8000/build/assets/fano2.jpg',
-    'http://127.0.0.1:8000/build/assets/fano3.jpg',
-    'http://127.0.0.1:8000/build/assets/fano4.jpg'
-    ]
-
-let banner = document.querySelector('#banner')
-let i = 0
-setInterval(() => {
-
-banner.setAttribute('src',photoArray[i])
-
-switch(true){
-case i ==4 : i = 0; break;
-case i < 4 : i++;break;
-}
-
-
-}, 4000);
-
-    </script>
+    <script src="{{ asset('js/home-photo.js') }}"></script>
     </body>
 
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -50,27 +50,5 @@
 
         </div>
       </div>
-      <script>
-            // codice jquery per il logout
-$(document).ready(function() {
-    $("#logout-button").on("click", function() {
-        var csrfToken = $('meta[name="csrf-token"]').attr('content');
-        var form = $('<form/>', {
-            method: 'POST',
-            action: '/logout',
-            style: 'display: none;'
-        });
-
-        var csrfInput = $('<input/>', {
-            type: 'hidden',
-            name: '_token',
-            value: csrfToken
-        });
-
-        form.append(csrfInput).appendTo('body');
-        form.submit();
-    });
-});
-
-      </script>
+      <script src="{{ asset('js/logout-jquery.js')}}"></script>
 </nav>
